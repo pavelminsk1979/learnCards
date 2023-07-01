45a247e956a992e92ff3836cb883f1e5ecc6adce
pavelminsk1979 <pavelminsk1979@mail.ru>
2023-07-01 15:46:59 +0300

    scss variable

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,6 @@
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/700.css';
+import './styles/index.scss'
 import { createRoot } from 'react-dom/client'
 
 import { App } from './App'
